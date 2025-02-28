5d4a35d21cc0d2da86f265878aeb581544f9c4db
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-02-28 18:51:43 +0100

    Bla

--- theory3.pl
+++ theory3.pl
@@ -8,4 +8,9 @@ strict(r1,knowledge,exam(X),[student(X),course(X,medicine)]).
 defeasible(r2,permission,diploma(X),exam(X)).
 
 run :-
-    run_query(pdelta(diploma(alice),permission)) .
+    run_query(pdelta(graduate(alice),permission)) ,
+    run_query(pdelta(graduate(alice),obligation)) ,
+    run_query(mdelta(graduate(alice),obligation)) ,
+    run_query(pdelta(graduate(bob),permission)) ,
+    run_query(pdelta(graduate(bob),obligation)) ,
+    run_query(mdelta(graduate(bob),obligation)) .
